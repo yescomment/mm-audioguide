dfde9ed7fa603f6b4c45d63b31facfdb8e968c45
Jacob Ford <j@cobford.com>
2024-06-19 00:47:00 -0400

    Actually, going to bulk find download URLs manually
    so I can wget in parallel

--- scripts/grasshopper-export/save.sh
+++ scripts/grasshopper-export/save.sh
@@ -7,17 +7,10 @@ if [[ ! -f extensionurls.txt ]]; then
 fi
 
 # Create a directory for the downloaded HTML files and MP3s
-mkdir -p downloaded_extensionhtml
-mkdir -p downloaded_greetings
+mkdir -p extensions_html
+mkdir -p greetings_mp3
 
 # Step 1: Download HTML pages listed in extensionurls.txt
 while IFS= read -r url; do
     wget --header="Cookie: __RequestVerificationToken=***REMOVED***; .GRASSHOPPERAUTH=***REMOVED***;" -P downloaded_html "$url"
 done < extensionurls.txt
-
-# Step 2: Parse each HTML file for URLs matching the regex and download the MP3 files
-for html_file in downloaded_extensions/*; do
-    grep -oE '/File/DownloadGreeting\?greetingID=.{8}-.{4}-.{4}-.{4}-.{12}' "$html_file" | while IFS= read -r mp3_url; do
-        wget -P downloaded_mp3 "https://nuui.us.grasshopper.com$mp3_url"
-    done
-done
