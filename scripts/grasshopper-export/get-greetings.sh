368b69ab16237b9e8044550e513e7e88596f643a
Jacob Ford <j@cobford.com>
2024-06-19 00:52:49 -0400

    Preserve filenames

--- scripts/grasshopper-export/get-greetings.sh
+++ scripts/grasshopper-export/get-greetings.sh
@@ -9,4 +9,4 @@ fi
 # Create a directory for the downloaded HTML files and MP3s
 mkdir -p greetings_mp3
 
-wget --header="Cookie: __RequestVerificationToken=***REMOVED***; .GRASSHOPPERAUTH=***REMOVED***;" -P greetings_mp3 -i greetingurls.txt
+wget --header="Cookie: __RequestVerificationToken=***REMOVED***; .GRASSHOPPERAUTH=***REMOVED***;" -P greetings_mp3 --content-disposition -i greetingurls.txt
